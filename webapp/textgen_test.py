ceb333dec196f2f8cf1e9544f1c9592d72264269
Ronald T <ronaldt@kth.se>
2016-02-06 15:05:05 +0200

    different font

--- webapp/textgen_test.py
+++ webapp/textgen_test.py
@@ -4,15 +4,15 @@ import numpy as np
 TEXT = "garage48"
 WINDOW_SIZE = (10, 20)
 STEP = 3
-FONT = cv2.FONT_HERSHEY_PLAIN
-FONT_SCALE = 0.7
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.38
 
 text_size = cv2.getTextSize(TEXT, FONT, FONT_SCALE, 1)
 print text_size
-img = np.ones((10, text_size[0][0] + 2*WINDOW_SIZE[1], 3), dtype=np.uint8)*255
+img = np.ones((12, text_size[0][0] + 2*WINDOW_SIZE[1], 3), dtype=np.uint8)*255
 
 cv2.putText(img, TEXT, (WINDOW_SIZE[1], 8), FONT,
-            FONT_SCALE,            # fontScale
+            FONT_SCALE,     # fontScale
             (255, 0, 0))    # BGR
 cv2.imwrite('/tmp/test.png', img)
 
