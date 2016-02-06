ba4b6cda500910b82c72d53c2b3ecfea2aa9be62
Ronald T <ronaldt@kth.se>
2016-02-06 17:20:43 +0200

    test_png_upload.py

---
+++ webapp/test_png_upload.py
@@ -0,0 +1,14 @@
+from ledscreen_connection import LedScreenConnection
+import cv2
+import sys
+import time
+
+img = cv2.imread(sys.argv[1])
+assert img.shape == (10, 10, 3)
+
+conn = LedScreenConnection('127.0.0.1')
+
+while not conn.connected:
+    time.sleep(0.1)
+conn.send_pixels(img.tostring())
+conn.keep_running = False
