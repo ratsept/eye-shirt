14c31a7239742db94481581dba460e952c527390
Ronald T <ronaldt@kth.se>
2016-02-07 17:23:28 +0200

    ledscreen_connection 10x20 mapping

--- webapp/ledscreen_connection.py
+++ webapp/ledscreen_connection.py
@@ -5,15 +5,45 @@ import numpy as np
 
 PORT = 2000
 
+MAPPING_STR = '''
+128 127 126 125 124 153 152 151 150 149
+10  11  30  31  50  51  68  69  88  89
+133 132 131 130 129 158 157 156 155 154
+9   12  29  32  49  52  67  70  87  90
+138 137 136 135 134 163 162 161 160 159
+8   13  28  33  48  53  66  71  86  91
+143 142 141 140 139 168 167 166 165 164
+7   14  27  34  47  54  65  72  85  92
+148 147 146 145 144 173 172 171 170 169
+6   15  26  35  46  55  64  73  84  93
+103 102 101 100 99  178 177 176 175 174
+5   16  25  36  45  56  x   74  83  94
+108 107 106 105 104 183 182 181 180 179
+4   17  24  37  44  57  x   75  82  95
+113 112 111 110 109 188 187 186 185 184
+3   18  23  38  43  58  63  76  81  96
+118 117 116 115 114 193 192 191 190 189
+2   19  22  39  42  59  62  77  80  97
+123 122 121 120 119 198 197 196 195 194
+1   20  21  40  41  60  61  78  79  98
+'''
+
+_MAP_STR_LIST = filter(lambda s: s != '', MAPPING_STR.split('\n'))
+_MAP = [s.split() for s in _MAP_STR_LIST]
+MAP = [[int(s) if s != 'x' else None for s in row] for row in _MAP]
+MAP_MAX = max([max(row) for row in MAP])
+
 
 def screen_mapping(image):
-    screendata = np.zeros(image.shape, dtype=np.uint8)
-    for row in range(screendata.shape[0]):
-        if row % 2:
-            screendata[row] = image[row]
-        else:
-            screendata[row] = image[row, ::-1, :]
-    return screendata
+    assert image.shape == (10, 20, 3)
+    buf = [0, 0, 0]*MAP_MAX
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            addr = MAP[col][row]
+            if addr:
+                addr = (addr-1)*3
+                buf[addr:addr+3] = image[row, col].tostring()
+    return ''.join(buf)
 
 
 class LedScreenConnection(threading.Thread):
@@ -52,4 +82,5 @@ class LedScreenConnection(threading.Thread):
 
     def send_image(self, image):
         assert self.connected
-        self._socket.send(screen_mapping(image).tostring())
+        self._buf = screen_mapping(image)
+        self._socket.send(self._buf)
